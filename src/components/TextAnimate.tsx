45e244ad29da963d7ef01d7ce924f91ab9c90d8a
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-04-13 11:57:42 +0600

    feat: add TextAnimate component with animated words for enhanced UI

---
+++ src/components/TextAnimate.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const wordsToAnimate = [
+  "Frontend Developer",
+  "Backend Developer",
+  "Full Stack Developer",
+  "Web UI Developer",
+  "JavaScript Developer",
+  "HTML/CSS Developer",
+  "React.js Developer",
+  "Web Application Developer",
+  "WordPress Web Developer",
+];
+
+const TextAnimate = () => {
+  return <div>TextAnimate</div>;
+};
+
+export default TextAnimate;
