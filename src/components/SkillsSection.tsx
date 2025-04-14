fb7bfe73aff2753dfa5f2998d78e0669581ff989
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-04-15 02:44:28 +0600

    fix: enhance SkillsSection styling for better accessibility and dark mode support

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -55,19 +55,23 @@ export default function SkillsSection() {
   const filteredSkills = activeTab === "all" ? skills : skills.filter((skill) => skill.category === activeTab);
 
   return (
-    <section className="pt-28 pb-20 container mx-auto">
+    <section className="pt-28 pb-20 min-h-[80vh] container mx-auto">
       <div className="mb-8">
-        <h2 className="md:text-5xl text-3xl underline font-bold text-center text-gray-800 mt-1">Tech Stack</h2>
+        <h2 className="md:text-5xl text-3xl underline font-bold text-center text-gray-800 dark:text-slate-100 mt-1">
+          Tech Stack
+        </h2>
       </div>
 
-      <div className="flex flex-wrap gap-2 mb-8 border-b">
+      <div className="flex flex-wrap gap-2 mb-8 border-b dark:border-gray-700 border-gray-300">
         {["all", "frontend", "backend", "tools"].map((tab) => (
           <button
             key={tab}
             onClick={() => setActiveTab(tab as "all" | "frontend" | "backend" | "tools")}
             className={cn(
               "px-4 py-2 font-medium text-lg transition-colors",
-              activeTab === tab ? "text-gray-800 border-b-2 border-gray-800" : "text-gray-500 hover:text-gray-700"
+              activeTab === tab
+                ? "text-gray-800 dark:text-white border-b-2 border-gray-800  dark:border-white"
+                : "text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
             )}
           >
             {tab.charAt(0).toUpperCase() + tab.slice(1)}
@@ -80,14 +84,14 @@ export default function SkillsSection() {
           {filteredSkills.map((skill, index) => (
             <motion.div
               key={skill.name}
-              className="flex flex-col items-center z-10 justify-center p-6 border border-gray-200 rounded-md bg-white hover:shadow-md transition-shadow"
+              className="flex flex-col items-center z-10 justify-center p-6 border border-gray-200 rounded-md bg-white hover:shadow-md transition-shadow duration-300 dark:bg-gray-900  dark:border-gray-700 dark:hover:shadow-lg"
               initial={{ opacity: 0, y: 100 }}
               animate={{ opacity: 1, y: 0 }}
               exit={{ opacity: 0, y: -100 }}
               transition={{ duration: 0.3, delay: index * 0.1 }}
             >
               {skill.icon}
-              <span className="mt-3 text-center text-gray-700">{skill.name}</span>
+              <span className="mt-3 text-center text-gray-700 dark:text-slate-300">{skill.name}</span>
             </motion.div>
           ))}
         </AnimatePresence>
