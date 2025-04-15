89ff0588233d08a3740fd68d2275125bc4fdc6be
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-04-16 03:21:10 +0600

    fix: update Projects page header and add outdated project warning in ProjectCard component

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -36,7 +36,9 @@ export default function Projects() {
     <section id="projects" className="py-20 ">
       <div className="container px-4 mx-auto">
         <div className="md:mb-16 mb-5 text-center">
-          <h2 className="text-3xl font-bold tracking-tight sm:text-4xl mb-4">My Projects</h2>
+          <h2 className="text-3xl font-bold tracking-tight sm:text-4xl  mb-4">My Projects</h2>
+          {/* outdated project warning and working on new projects */}
+
           <p className="max-w-2xl mx-auto text-muted-foreground">
             A showcase of my recent work, personal projects, and collaborations.
           </p>
