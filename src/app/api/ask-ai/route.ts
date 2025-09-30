6d5c0832c9208ae231f6164168a6f1f418ea3c03
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-09-30 16:11:09 +0600

    test commit

--- src/app/api/ask-ai/route.ts
+++ src/app/api/ask-ai/route.ts
@@ -45,6 +45,7 @@ export async function POST(req: NextRequest) {
       messages,
     });
     // console.log(completion);
+    
 
     const responseMessage = completion.choices[0]?.message?.content;
     console.log(responseMessage);
