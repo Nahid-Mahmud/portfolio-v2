af7a05dbeb0d6f425e6233a356501663d2f97483
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-11-05 01:30:04 +0600

    feat: add NEXT_PUBLIC_BASE_URL to environment variables configuration

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,6 @@
 const envVariables = {
   NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL as string,
+  NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL as string,
 };
 
 export default envVariables;
