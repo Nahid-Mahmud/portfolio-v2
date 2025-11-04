e53eff3af24b652cfeeb819f2c80178b12557f74
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-11-05 02:16:23 +0600

    feat: enhance image optimization settings in next.config.ts and add quality attributes in page.tsx

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -141,6 +141,10 @@ export default function Home() {
             fill
             className="object-cover z-10"
             priority={true}
+            quality={85}
+            sizes="(max-width: 768px) 256px, (max-width: 1024px) 320px, (max-width: 1280px) 384px, 500px"
+            placeholder="blur"
+            blurDataURL="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAABAAEDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAv/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWGRkqGx0f/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAGhEAAgIDAAAAAAAAAAAAAAAAAAECEgMRkf/aAAwDAQACEQMRAD8AltJagyeH0AthI5xdrLcNM91BF5pX2HaH9bcfaSXWGaRmknyLli5xplGnpQcPzfUAUAF1NXDTuADjCDV4y6mNLKNUx8AZLMVQE3rKrU5lcKm17nZM7e+jcJHmwERJ5gOzHJkzrE3xvUAPDMILrPanrGaXMJEGsYF0YfPMl8mY9xZGLAZv"
           />
         </div>
       </div>
